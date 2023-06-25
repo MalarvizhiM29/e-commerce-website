884a81577a8e522f1050006734ece06585dfde68
MALARVIZHI M <Malarm2004@gmail.com>
2023-06-25 14:49:12 +0530

    new commit

---
+++ main.js
@@ -0,0 +1,72 @@
+let search = document.querySelector(".search-box");
+
+document.querySelector("#search-icon").onclick = () => {
+  search.classList.toggle("active");
+  cart.classList.remove("active");
+  user.classList.remove("active");
+  navbar.classList.remove("active");
+};
+
+let cart = document.querySelector(".cart");
+
+document.querySelector("#cart-icon").onclick = () => {
+  cart.classList.toggle("active");
+  search.classList.remove("active");
+  user.classList.remove("active");
+  navbar.classList.remove("active");
+};
+
+let user = document.querySelector(".user");
+
+document.querySelector("#user-icon").onclick = () => {
+  user.classList.toggle("active");
+  search.classList.remove("active");
+  cart.classList.remove("active");
+  navbar.classList.remove("active");
+};
+
+let navbar = document.querySelector(".navbar");
+
+document.querySelector("#menu-icon").onclick = () => {
+  navbar.classList.toggle("active");
+  search.classList.remove("active");
+  cart.classList.remove("active");
+  user.classList.remove("active");
+};
+
+window.onscroll = () => {
+  search.classList.remove("active");
+  cart.classList.remove("active");
+  user.classList.remove("active");
+  navbar.classList.remove("active");
+};
+
+let header = document.querySelector("header");
+
+window.addEventListener("scroll", () => {
+  header.classList.toggle("shadow", window.scrollY > 0);
+});
+
+var swiper = new Swiper(".new-arrival", {
+  spaceBetween: 20,
+  loop: true,
+  autoplay: {
+    delay: 5500,
+    disableOnInteraction: false,
+  },
+  centeredSlides: true,
+  breakpoints: {
+    0: {
+      slidesPreView: 0,
+    },
+    568: {
+      slidesPerView: 2,
+    },
+    768: {
+      slidesPerView: 2,
+    },
+    1020: {
+      slidesPerView: 3,
+    },
+  },
+});
